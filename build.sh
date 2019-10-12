b29cfc60a3e1dc8fcbd0a21811eb8a584f9265ed
Yi Wang <wangyihit@hotmail.com>
2019-10-12 14:48:55 +0800

    add mac version

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+# go tool dist list 
+
+echo "build for win"
 GOOS=windows GOARCH=386 go build -o echo_server.exe
+echo "build for mac"
+GOOS=darwin GOARCH=amd64 go build -o echo_server_mac
+echo "build for linux"
 go build -o echo_server
+echo "complete"
+
