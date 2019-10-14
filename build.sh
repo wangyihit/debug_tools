96387919d4e3278a35002975960d276c31526ef5
Yi Wang <wangyihit@hotmail.com>
2019-10-14 11:36:11 +0800

    add binary for resbarry pi

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ echo "build for win"
 GOOS=windows GOARCH=386 go build -o echo_server.exe
 echo "build for mac"
 GOOS=darwin GOARCH=amd64 go build -o echo_server_mac
+echo "build for pi"
+GOOS=linux GOARCH=arm go build -o echo_server_pi
 echo "build for linux"
 go build -o echo_server
 echo "complete"
